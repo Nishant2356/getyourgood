1ee2d4ede910b2171d1ac797e55427fe5ffd5345
Nishant2356 <nm2356opp@gmail.com>
2025-11-15 20:40:27 +0530

    favicon-change

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,16 +7,16 @@ import { SessionProvider } from 'next-auth/react';
 import Navbar from "@/components/Navbar";
 import { Toaster } from "sonner";
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <html lang="en">
+        <head>
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/favicon.png" />
+          <meta name="theme-color" content="#22c55e" />
+        </head>
         <body>
-          {/* <Navbar /> */}
           {children}
           <Toaster position="top-center" richColors />
         </body>
